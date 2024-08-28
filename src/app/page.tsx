bca4a2b23487e1a9f0ddccd72c5afc12c089daf9
Jakub Jankiewicz <jcubic@onet.pl>
2024-08-29 00:26:51 +0200

    add footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,13 +7,18 @@ import UploadForm from '@/components/UploadForm';
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <h1>FreeCodeCamp - GitHub image hosting</h1>
-      <Suspense fallback={<p>loading...</p>}>
-        <Gallery />
-      </Suspense>
-      <h2>Upload form</h2>
-      <UploadForm/>
-    </main>
+    <>
+      <main className={styles.main}>
+        <h1>FreeCodeCamp - GitHub image hosting</h1>
+        <Suspense fallback={<p>loading...</p>}>
+          <Gallery />
+        </Suspense>
+        <h2>Upload form</h2>
+        <UploadForm/>
+      </main>
+      <footer>
+        <p>Copyright (C) <a href="https://jakub.jankiewicz.org/">Jakub T. Jankiewicz</a>, source on <a href="https://github.com/jcubic/next-github-hosting">GitHub</a></p>
+      </footer>
+    </>
   );
 }
